98aac81c9282dc8a87f2914170ed7be7833d475e
Your Name <you@example.com>
2025-01-16 14:43:51 +0300

    lk

--- service-worker.js
+++ service-worker.js
@@ -1,47 +1,44 @@
 const CACHE_NAME = "gpa-calculator-cache-v1";
 const urlsToCache = [
-    "./",
-    "./Home.html",
-    "./style.css",
-    "./logic.js",
-    "./manifest.json",
-    "./img/Retro_Dazle_Night_Club_Logo.png",
-    "./img/AAAA.gif"
+  "./",
+  "./Home.html",
+  "./style.css",
+  "./logic.js",
+  "./manifest.json",
+  "./icon-192x192.png",
+  "./icon-512x512.png"
 ];
 
 // تثبيت Service Worker
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            console.log("Caching files...");
-            return cache.addAll(urlsToCache);
-        })
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-// جلب الملفات من الكاش أو الشبكة
+// جلب الملفات من الكاش
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request).catch(() => {
-                console.error("Resource not available offline:", event.request.url);
-            });
-        })
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
 
-// تحديث الكاش عند الحاجة
+// تحديث الكاش عند حدوث تغييرات
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        console.log("Deleting old cache:", cacheName);
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames =>
+      Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
         })
-    );
+      )
+    )
+  );
 });
