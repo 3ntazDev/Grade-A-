5a9c55ad97fa641f5a9ac05c343943d2a250e65f
Your Name <you@example.com>
2025-01-16 14:40:27 +0300

    k

--- service-worker.js
+++ service-worker.js
@@ -5,10 +5,11 @@ const urlsToCache = [
     "./style.css",
     "./logic.js",
     "./manifest.json",
-    "./img/Retro_Dazle_Night_Club_Logo-removebg-preview.png",
+    "./img/Retro_Dazle_Night_Club_Logo.png",
     "./img/AAAA.gif"
 ];
 
+// تثبيت Service Worker
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -18,38 +19,29 @@ self.addEventListener("install", event => {
     );
 });
 
-
-
-
+// جلب الملفات من الكاش أو الشبكة
 self.addEventListener("fetch", event => {
-  event.respondWith(
-      fetch(event.request).then(response => {
-          if (response.redirected) {
-              return Response.error();
-          }
-          return response;
-      }).catch(() => {
-          return caches.match(event.request);
-      })
-  );
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request).catch(() => {
+                console.error("Resource not available offline:", event.request.url);
+            });
+        })
+    );
 });
 
-
-
-
-// تحديث الكاش عند حدوث تغييرات
+// تحديث الكاش عند الحاجة
 self.addEventListener("activate", event => {
-  event.waitUntil(
-      caches.keys().then(cacheNames => {
-          return Promise.all(
-              cacheNames.map(cacheName => {
-                  if (cacheName !== CACHE_NAME) {
-                      console.log("Deleting old cache:", cacheName);
-                      return caches.delete(cacheName);
-                  }
-              })
-          );
-      })
-  );
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log("Deleting old cache:", cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
-
